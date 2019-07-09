31e61415a9e0ab1b8ebf0642516958bc7c31428d
Florencia Peretti <floren.peretti@knowbly.com>
2019-07-09 00:59:11 -0300

    added part 3

---
+++ part3/phonebook/src/components/NumberForm.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const FormInput = ({label, value, onChange}) => (
+    <div>
+        {label}: <input value={value} onChange={onChange} />
+    </div>
+)
+
+const NumberForm = ({onSubmit, inputs}) => {
+    const formInputs = () => inputs.map(
+        ({label, value, onChange}) => <FormInput key={label} label={label} value={value} onChange={onChange} />
+    )
+
+    return (
+        <form onSubmit={onSubmit}>
+            {formInputs()}
+            <div>
+                <button type="submit">add</button>
+            </div>
+        </form>
+    )
+}
+
+export default NumberForm
